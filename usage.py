1f05909a0c51e3f530b8c7889ed7727870cd1f60
Taehun Kim <taehoon1018@postech.ac.kr>
2022-11-23 17:58:44 +0900

    update usage for video

--- usage.py
+++ usage.py
@@ -1,17 +1,43 @@
+import cv2
+
 from PIL import Image
 from transparent_background import Remover
 
 remover = Remover()
-img = Image.open('figures/sample.jpg')
 
-# default setting - transparent background
-out = remover.process(img)
-Image.fromarray(out).save('output.png')
+# Usage for image
+img = Image.open('samples/aeroplane.jpg') # read image
 
-# object map only
-out = remover.process(img, type='map')
-Image.fromarray(out).save('output.png')
+out = remover.process(img) # default setting - transparent background
+out = remover.process(img, type='rgba') # same as above
+out = remover.process(img, type='map') # object map only
+out = remover.process(img, type='green') # image matting - green screen
+out = remover.process(img, type='blur') # blur background
+out = remover.process(img, type='overlay') # overlay object map onto the image
+out = remover.process(img, type='samples/background.jpg') # use another image as a background
 
-# image matting - green screen
-out = remover.process(img, type='green')
-Image.fromarray(out).save('output.png')
+Image.fromarray(out).save('output.png') # save result
+
+# Usage for video
+cap = cv2.VideoCapture('samples/b5.mp4') # video reader for input
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+writer = None
+
+while cap.isOpened():
+    ret, frame = cap.read() # read video
+
+    if ret is False:
+        break
+        
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    img = Image.fromarray(frame).convert('RGB')
+
+    if writer is None:
+        writer = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, img.size) # video writer for output
+
+    out = remover.process(img, type='map') # same as image, except for 'rgba' which is not for video.
+    writer.write(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
+
+cap.release()
+writer.release()
