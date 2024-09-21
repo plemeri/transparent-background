f5982d096ca15dbd5d2e01b2396ff09431ad95fb
Kamal Kamalaldin <kamal.aldin.94@gmail.com>
2024-09-21 16:42:58 -0400

    Format install script

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import os
 import shutil
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-cfg_path = os.environ.get('TRANSPARENT_BACKGROUND_FILE_PATH', os.path.abspath(os.path.expanduser('~')))
-os.makedirs(os.path.join(cfg_path, '.transparent-background'), exist_ok=True)
+cfg_path = os.environ.get(
+    "TRANSPARENT_BACKGROUND_FILE_PATH", os.path.abspath(os.path.expanduser("~"))
+)
+os.makedirs(os.path.join(cfg_path, ".transparent-background"), exist_ok=True)
 
 setuptools.setup(
     name="transparent-background",
@@ -17,22 +19,37 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/plemeri/transparent-background",
-    packages=['transparent_background', 'transparent_background.modules', 'transparent_background.backbones'],
+    packages=[
+        "transparent_background",
+        "transparent_background.modules",
+        "transparent_background.backbones",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
-    install_requires=['torch>=1.7.1', 'torchvision>=0.8.2', 'opencv-python>=4.6.0.66', 'timm>=0.6.11', 'tqdm>=4.64.1', 'kornia>=0.5.4', 'gdown>=4.5.4', 'wget>=3.2', 'easydict>=1.10', 'pyyaml>=6.0', 'albumentations>=1.3.1', 'flet>=0.23.1'],
-    extras_require={
-        'webcam': ['pyvirtualcam>=0.6.0']
-    },
+    python_requires=">=3.8",
+    install_requires=[
+        "torch>=1.7.1",
+        "torchvision>=0.8.2",
+        "opencv-python>=4.6.0.66",
+        "timm>=0.6.11",
+        "tqdm>=4.64.1",
+        "kornia>=0.5.4",
+        "gdown>=4.5.4",
+        "wget>=3.2",
+        "easydict>=1.10",
+        "pyyaml>=6.0",
+        "albumentations>=1.3.1",
+        "flet>=0.23.1",
+    ],
+    extras_require={"webcam": ["pyvirtualcam>=0.6.0"]},
     entry_points={
         "console_scripts": [
             "transparent-background=transparent_background:console",
-            "transparent-background-gui=transparent_background:gui"
+            "transparent-background-gui=transparent_background:gui",
         ],
     },
-    include_package_data=True
+    include_package_data=True,
 )
